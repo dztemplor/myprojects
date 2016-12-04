0e210915d65f18421b12306f48548f16bf4b9bb3
Spike Du <Zhan.Du@technicolor.com>
2016-12-04 17:19:11 +0800

    add changes to common.h and queue.h

--- include/common.h
+++ include/common.h
@@ -16,9 +16,28 @@
  #include <sys/types.h>
      #include <sys/stat.h>
 #include <semaphore.h>
- #include <sys/queue.h>
+//#include <sys/queue.h>
+#include "queue.h"
 
+#include <sys/file.h>
 
        #include <fcntl.h>
+
+       #include <semaphore.h>
+ #include <time.h>
+
+
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+
+union semun {
+               int              val;    /* Value for SETVAL */
+               struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+               unsigned short  *array;  /* Array for GETALL, SETALL */
+               struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                                           (Linux-specific) */
+           };
+
 
 #endif
